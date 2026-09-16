4d5ecd64c22b8ef33d4202dd46055614e1602d46
Owenoz <owenozmubb07@gmail.com>
2026-09-16 13:17:02 +0300

    chore: bump versionCode to 2 for APK rebuild

--- mobile-movie-app/app.config.js
+++ mobile-movie-app/app.config.js
@@ -23,7 +23,7 @@ export default {
         backgroundColor: "#030014",
       },
       package: "com.moviehub.app",
-      versionCode: 1,
+      versionCode: 2,
       permissions: ["INTERNET", "ACCESS_NETWORK_STATE"],
     },
     web: {
